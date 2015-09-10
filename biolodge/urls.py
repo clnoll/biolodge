e2bc28f53216da6ca0aa4b7521089b888eab1bc8
Catherine Noll <catherine.noll@ampush.com>
2015-09-10 04:22:51 -0700

    initial url

--- biolodge/urls.py
+++ biolodge/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from biolodge.birds.views import get_birds
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'', get_birds),
 ]
