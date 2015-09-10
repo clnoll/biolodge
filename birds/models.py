eab15b11b5b3b92421b86d4fed621b0d1cb27909
Catherine Noll <catherine.noll@ampush.com>
2015-09-10 03:46:34 -0700

    birds models

--- birds/models.py
+++ birds/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+
+    name = models.CharField(null=False, unique=True)
+    polygon = models.TextField()
+
+
+class Subspecies(models.Model):
+
+    order = models.CharField()
+    family = models.CharField()
+    genus = models.CharField()
+    species = models.CharField()
+    subspecies = models.CharField()
+    raw_location = models.TextField()
+    ebird_id = models.CharField(unique=True)
+    common_name = models.CharField()
+
+    locations = models.ManyToManyField(Location)
