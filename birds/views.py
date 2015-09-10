981667a02f5387ada89d694cb8b0aa8c5d258b2a
Catherine Noll <catherine.noll@ampush.com>
2015-09-10 04:59:50 -0700

    initial birds table

--- birds/views.py
+++ birds/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 
 def get_birds(request):
-    data = {}
+    header = ['field', 'another', 'third']
+    body = [['b1', 'b2', 'b3'],
+            ['a', 'b', 'c'],
+            ['afd', 'adfasdf', 'adfasdfasf']]
+    data = {
+        'header': header,
+        'body': body,
+    }
     return render(request, 'birds/birds.html', data)
