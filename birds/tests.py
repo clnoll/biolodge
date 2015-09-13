e9780b7e855805ecf0abb8eb1a14492d1ab51ee9
Catherine Noll <catherine.noll@ampush.com>
2015-09-13 05:11:22 -0700

    WIP:  birds.tests

--- birds/tests.py
+++ birds/tests.py
@@ -1,74 +1,79 @@
 from django.test import TestCase
 
-import parse_range
-from birds.data import read_ebird_csv
+import range_grammar
+# from birds.data import read_ebird_csv
 
 
 class TestRangeGrammar(TestCase):
 
-    # Test parse_range.preprocess
+    # Test range_grammar.preprocess
     def test_preprocess_cardinal(self):
         test_string = "S Kenya and e Tanzania"
-        result = parse_range.preprocess(test_string)
-        self.assertEquals(result, "southern Kenya and eastern Tanzania")
+        result = range_grammar.preprocess(test_string)
+        self.assertEquals(result, "southern kenya and eastern tanzania")
 
     def test_preprocess_ordinal(self):
         test_string = "Desert puna of se Peru, sw Bolivia and nw Argentina"
-        result = parse_range.preprocess(test_string)
-        self.assertEquals(result, "Desert puna of southeastern Peru, southwestern Bolivia and northwestern Argentina")
+        result = range_grammar.preprocess(test_string)
+        self.assertEquals(result, "desert puna of southeastern peru, southwestern bolivia and northwestern argentina")
 
     def test_preprocess_multiple_ranges(self):
         test_string = "S Ethiopia to Somalia and adjacent ne Kenya"
-        result = parse_range.preprocess(test_string)
-        self.assertEquals(result, "southern Ethiopia to Somolia and adjacent northeastern Kenya")
+        result = range_grammar.preprocess(test_string)
+        self.assertEquals(result, "southern ethiopia to somalia and adjacent northeastern kenya")
 
     def test_preprocess_no_adjectives(self):
         test_string = "Andes of central Peru to Bolivia"
-        result = parse_range.preprocess(test_string)
-        self.assertEquals(result, test_string)
+        result = range_grammar.preprocess(test_string)
+        self.assertEquals(result, test_string.lower())
 
-    # Test parse_range.make_grammar
-    def test_make_grammar_conjunction(self):
-        test_string = "S Chile, s Argentina and Falkland Islands"
-        result = parse_range.make_grammar(test_string)
-        expected_result = []
-        self.assertEquals(result, expected_result)
+    # # Test range_grammar.make_grammar
+    # def test_make_grammar_conjunction(self):
+    #     test_string = "S Chile, s Argentina and Falkland Islands"
+    #     grammar = range_grammar.make_grammar()
+    #     test_string = range_grammar.preprocess(test_string)
+    #     # import ipdb; ipdb.set_trace()
+    #     result = grammar.parseString(test_string)
+    #     expected_result = [['southern', 'chile'],
+    #                        ['southern', 'argentina'],
+    #                        ['falkland islands']]
+    #     self.assertEquals(result, expected_result)
 
-    def test_make_grammar_conjunction_fill(self):
-        test_string = "Costa Rica to Brazil; Africa, Madagascar and Comoro Islands"
-        result = parse_range.make_grammar(test_string)
-        expected_result = []
-        self.assertEquals(result, expected_result)
+    # def test_make_grammar_conjunction_fill(self):
+    #     test_string = "Costa Rica to Brazil; Africa, Madagascar and Comoro Islands"
+    #     result = range_grammar.make_grammar(test_string)
+    #     expected_result = [['Costa Rica', 'to', 'Brazil'], ['Africa'], ['Madagascar', 'Comoro Islands']]
+    #     self.assertEquals(result, expected_result)
 
-    def test_make_grammar_semicolon(self):
-        test_string = "French Guiana and extreme ne Brazil; e Peru and nw Brazil"
-        result = parse_range.make_grammar(test_string)
-        expected_result = []
-        self.assertEquals(result, expected_result)
+    # def test_make_grammar_semicolon(self):
+    #     test_string = "French Guiana and extreme ne Brazil; e Peru and nw Brazil"
+    #     result = range_grammar.make_grammar(test_string)
+    #     expected_result = [['French Guiana'], ['extreme', 'northeastern', 'Brazil'], ['eastern Peru', 'northwestern Brazil']]
+    #     self.assertEquals(result, expected_result)
 
-    def test_make_grammar_single_fill(self):
-        test_string = "E Bolivia to Paraguay, s Brazil and n Argentina"
-        result = parse_range.make_grammar(test_string)
-        expected_result = []
-        self.assertEquals(result, expected_result)
+    # def test_make_grammar_single_fill(self):
+    #     test_string = "E Bolivia to Paraguay, s Brazil and n Argentina"
+    #     result = range_grammar.make_grammar(test_string)
+    #     expected_result = [['eastern Bolivia', 'to', 'Paraguay'], ['southern Brazil', 'northern Argentina']]
+    #     self.assertEquals(result, expected_result)
 
-    def test_make_grammar_multiple_fill(self):
-        test_string = "lowlands from northern Mexico south, west of the Andes, to southwestern Ecuador and, east of the Andes, to northeastern Argentina and Brazil"
-        result = parse_range.make_grammar(test_string)
-        expected_result = []
-        self.assertEquals(result, expected_result)
+    # def test_make_grammar_multiple_fill(self):
+    #     test_string = "lowlands from northern Mexico south, west of the Andes, to southwestern Ecuador and, east of the Andes, to northeastern Argentina and Brazil"
+    #     result = range_grammar.make_grammar(test_string)
+    #     expected_result = [['lowlands', 'from', 'northern Mexico'], ['south'], ['west', 'of', 'the', 'Andes', 'to', 'southwestern Ecuador'], ['east', 'of', 'the', 'Andes', 'to', 'northeastern Argentina', ['Brazil']]]
+    #     self.assertEquals(result, expected_result)
 
-    def test_make_grammar_parenthetical_with_fill(self):
-        test_string = "Coastal se Brazil (Minas Gerais to Rio Grande do Sul)"
-        result = parse_range.make_grammar(test_string)
-        expected_result = []
-        self.assertEquals(result, expected_result)
+    # def test_make_grammar_parenthetical_with_fill(self):
+    #     test_string = "Coastal se Brazil (Minas Gerais to Rio Grande do Sul)"
+    #     result = range_grammar.make_grammar(test_string)
+    #     expected_result = []
+    #     self.assertEquals(result, expected_result)
 
-    def test_make_grammar_parenthetical_with_conjunction(self):
-        test_string = "Andes of nw Argentina (Jujuy and La Rioja)"
-        result = parse_range.make_grammar(test_string)
-        expected_result = []
-        self.assertEquals(result, expected_result)
+    # def test_make_grammar_parenthetical_with_conjunction(self):
+    #     test_string = "Andes of nw Argentina (Jujuy and La Rioja)"
+    #     result = range_grammar.make_grammar(test_string)
+    #     expected_result = []
+    #     self.assertEquals(result, expected_result)
 
 
 class TestReadData(TestCase):
