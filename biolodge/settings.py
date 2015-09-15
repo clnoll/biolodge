781639a83d8ea052c2671525b3e2e17c89c1285d
Catherine Noll <catherine.noll@ampush.com>
2015-09-14 23:23:43 -0700

    WIP: configure geodjango

--- biolodge/settings.py
+++ biolodge/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'birds',
+    'geo',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,6 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'geo': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.path.join(BASE_DIR, 'geo.postgis'),
     }
 }
 
