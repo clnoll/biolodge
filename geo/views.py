4756ecaf84d68bd1689e1b780baf16268afa0b34
Dan Davison <dan@counsyl.com>
2015-09-23 01:29:39 -0700

    Initial attempt at rendering bird ranges

--- geo/views.py
+++ geo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from birds.forms import RangeForm
 from birds.models import Bird
 from geo.models import WorldBorder
 
@@ -8,16 +9,17 @@ from geo.models import WorldBorder
 class MapView(View):
 
     def get(self, request):
-        # import ipdb; ipdb.set_trace()
+        borders = set(s.lower() for s in WorldBorder.objects.values_list('name', flat=True))
 
-        borders = set(WorldBorder.objects.values_list('name', flat=True))
-
-        species_with_known_range = Bird.objects.exclude(parsed_range='')
+        species_with_known_range = (Bird.objects
+                                    .exclude(parsed_range='')
+                                    .exclude(common_name=''))
 
         species_with_known_range = species_with_known_range[:20]
 
         data = {
             'birds': {},
+            'form_media': RangeForm().media,
         }
 
         birds = data['birds']
@@ -32,7 +34,29 @@ class MapView(View):
             for region in species_regions:
                 if region in borders:
                     birds[name]['matched_species_polys'].append(region)
+                    region = WorldBorder.objects.get(name__iexact=region)
                 else:
                     birds[name]['unmatched_species_polys'].append(region)
+
+            if birds[name]['matched_species_polys']:
+                region_names = birds[name]['matched_species_polys']
+                mpoly = WorldBorder.objects.get(name__iexact=region_names[0]).mpoly
+                for region_name in region_names[1:]:
+                    mpoly += WorldBorder.objects.get(name__iexact=region_name).mpoly
+                form = RangeForm(data={
+                    'name': name,
+                    'mpoly': mpoly,
+                })
+                birds[name]['form'] = form
+            else:
+                birds[name]['form'] = None
+
+            print 'Created entry: name=%s, form=%s,%s, #regions=%d' % (
+                name,
+                bool(form),
+                id(form),
+                len(birds[name]['matched_species_polys']),
+            )
+
 
         return render(request, 'geo/geo.html', data)
