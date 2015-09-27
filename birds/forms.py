896ed99a3a759ee0a30b252e93bfa63d831e60e7
Dan Davison <dan@counsyl.com>
2015-09-26 23:02:10 -0700

    Delete name field on form

--- birds/forms.py
+++ birds/forms.py
@@ -2,5 +2,4 @@ from django.contrib.gis import forms
 
 
 class RangeForm(forms.Form):
-    name = forms.CharField()
-    mpoly = forms.MultiPolygonField()
+    mpoly = forms.MultiPolygonField(label='')
