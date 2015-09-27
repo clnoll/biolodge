215a6f3a24f606f1a102c108f75dbc306fb7021c
Dan Davison <dan@counsyl.com>
2015-09-26 20:48:38 -0700

    Reorganize code and endpoints

--- biolodge/urls.py
+++ biolodge/urls.py
@@ -2,14 +2,13 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib.gis import admin
 
-from birds.views import Birds
-from birds.views import map_view
-from geo.views import MapView
+from birds.views import BirdListAPIView
+from birds.views import BirdListView
 
 
 urlpatterns = [
-    url(r'^$', Birds.as_view()),
-    url(r'map/', MapView.as_view()),
-    url(r'^map2/', map_view),
+    url(r'^$', BirdListView.as_view()),
+    url(r'^birds/', BirdListView.as_view()),
+    url(r'^api/birds/', BirdListAPIView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- birds/views.py
+++ birds/views.py
@@ -1,4 +1,7 @@
+import operator
+
 from django.shortcuts import render
+from django.views.generic import View
 from rest_framework import generics
 from rest_framework.serializers import ModelSerializer
 
@@ -12,15 +15,56 @@ class BirdSerializer(ModelSerializer):
         model = Bird
 
 
-class Birds(generics.ListAPIView):
+class BirdListAPIView(generics.ListAPIView):
     queryset = Bird.objects.all()
     serializer_class = BirdSerializer
 
 
-def map_view(request):
-    region = WorldBorder.objects.get(name='Indonesia')
-    form = RangeForm(data={
-        'name': region.name,
-        'mpoly': region.mpoly,
-    })
-    return render(request, 'birds/map.html', {'form': form})
+class BirdListView(View):
+
+    def get(self, request):
+        world_borders = {
+            border.name.lower(): border
+            for border in WorldBorder.objects.all()
+        }
+
+        birds = (Bird.objects
+                 .exclude(parsed_range='')
+                 .exclude(common_name=''))
+
+        birds = birds[:20]  # TODO: pagination
+
+        bird_dicts = []
+        for bird in birds:
+            bird_regions = set(bird.parsed_range['region_atoms'])
+
+            bird_dict = {
+                'name': bird.common_name,
+                'matched_regions': bird_regions & set(world_borders),
+                'unmatched_regions': bird_regions - set(world_borders),
+            }
+
+            if bird_dict['matched_regions']:
+                bird_world_borders = [
+                    world_borders[region_name]
+                    for region_name in bird_dict['matched_regions']]
+                bird_dict['form'] = _get_range_form(bird, bird_world_borders)
+            else:
+                bird_dict['form'] = None
+
+            bird_dicts.append(bird_dict)
+
+        data = {
+            'birds': bird_dicts,
+            'form_media': RangeForm().media,
+        }
+        return render(request, 'birds/bird_list.html', data)
+
+
+def _get_range_form(bird, world_borders):
+    form_data = {
+        'mpoly': reduce(operator.add, (border.mpoly
+                                       for border in world_borders)),
+    }
+    return RangeForm(data=form_data,
+                     auto_id='bird_%d_%%s' % bird.id)
