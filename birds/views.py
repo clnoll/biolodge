cf536f34d4bb428d2e317142ea06e4d0b0759e62
Catherine Noll <catherine.noll@ampush.com>
2015-09-27 01:05:34 -0700

    Messy implementation of Bird Detail View

--- birds/views.py
+++ birds/views.py
@@ -23,10 +23,37 @@ class BirdListAPIView(generics.ListAPIView):
 class BirdDetailView(View):
 
     def get(self, request, pks):
+        world_borders = {
+            border.name.lower(): border
+            for border in WorldBorder.objects.all()
+        }
+
         queryset = Bird.objects.filter(pk__in=pks.split(','))
+        birds = _get_map_data(queryset=queryset)
+
+        concat_birds = {'names': [],
+                        'ids': '',
+                        'matched_regions': set([]),
+                        'unmatched_regions': set([]),
+                        'form': '',
+                        }
+
+        for bird in birds:
+            concat_birds['names'].append(bird['name'])
+            concat_birds['ids'] = '%s_%s' % (concat_birds['ids'], bird['_id'])
+            concat_birds['matched_regions'] = concat_birds['matched_regions'] | bird['matched_regions']
+            concat_birds['unmatched_regions'] = concat_birds['unmatched_regions'] | bird['unmatched_regions']
+
+        if concat_birds['matched_regions']:
+            bird_world_borders = [
+                world_borders[region_name]
+                for region_name in concat_birds['matched_regions']]
+            concat_birds['form'] = _get_range_form(concat_birds, bird_world_borders, concat=True)
+        else:
+            concat_birds['form'] = None
 
         data = {
-            'birds': _get_map_data(queryset=queryset),
+            'birds': concat_birds,
             'form_media': RangeForm().media,
         }
         return render(request, 'birds/details.html', data)
@@ -48,8 +75,9 @@ def _get_map_data(queryset=None):
         for border in WorldBorder.objects.all()
     }
 
-    if all([query._meta.object_name == 'Bird' for query in queryset]):
-        birds = queryset
+    if queryset:
+        if all([query._meta.object_name == 'Bird' for query in queryset]):
+            birds = queryset
     else:
         birds = (Bird.objects
                  .exclude(parsed_range='')
@@ -64,6 +92,7 @@ def _get_map_data(queryset=None):
             bird_regions = set()
 
         bird_dict = {
+            '_id': bird.id,
             'name': bird.common_name,
             'matched_regions': bird_regions & set(world_borders),
             'unmatched_regions': bird_regions - set(world_borders),
@@ -82,10 +111,13 @@ def _get_map_data(queryset=None):
     return bird_dicts
 
 
-def _get_range_form(bird, world_borders):
-    form_data = {
-        'mpoly': reduce(operator.add, (border.mpoly
-                                       for border in world_borders)),
-    }
-    return RangeForm(data=form_data,
-                     auto_id='bird_%d_%%s' % bird.id)
+def _get_range_form(bird, world_borders, concat=False):
+        form_data = {
+            'mpoly': reduce(operator.add, (border.mpoly
+                                           for border in world_borders)),
+        }
+        if concat:
+            return RangeForm(data=form_data,
+                             auto_id='bird_%s_%%s' % bird['ids'])
+        return RangeForm(data=form_data,
+                         auto_id='bird_%d_%%s' % bird.id)
