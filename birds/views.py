54eec9d168e901d0b7dffa56bb8accc49967c2f1
Dan Davison <dan@counsyl.com>
2015-09-26 23:44:16 -0700

    Add columns and use bootstrap CSS

--- birds/views.py
+++ birds/views.py
@@ -49,6 +49,8 @@ class BirdListView(View):
 
             bird_dict = {
                 'name': bird.common_name,
+                'raw_range': bird.raw_range,
+                'parsed_range': bird.parsed_range,
                 'matched_regions': bird_regions & set(world_borders),
                 'unmatched_regions': bird_regions - set(world_borders),
             }
