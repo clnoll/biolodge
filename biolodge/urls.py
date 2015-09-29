6defd2fbe6e854478acbb46f702d35aa9ee827f2
Catherine Noll <catherine.noll@ampush.com>
2015-09-28 22:31:42 -0700

    Fix url for BirdListView

--- biolodge/urls.py
+++ biolodge/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^$', BirdListView.as_view()),
     url(r'^birds/(?P<pks>[\d,]+)/$', BirdDetailView.as_view()),
     url(r'^api/birds/$', BirdListAPIView.as_view()),
-    url(r'^birds/', BirdListView.as_view(), name='bird_list'),
+    url(r'^birds/$', BirdListView.as_view(), name='bird_list'),
     url(r'^admin/', include(admin.site.urls)),
 ]
