b9c90412eb17de8e3f48612593b50bf1fb9e0b60
Dan Davison <dan@counsyl.com>
2015-10-12 22:50:27 -0700

    Reference view classes using module

--- biolodge/urls.py
+++ biolodge/urls.py
@@ -2,22 +2,16 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib.gis import admin
 
-from birds.views import BirdDetailAPIView
-from birds.views import BirdDetailAPIViewAST
-from birds.views import BirdDetailViewAST
-from birds.views import BirdListAPIView
-from birds.views import BirdListView
-from birds.views import BirdRangeAPIViewAST
-from birds.views import WorldBordersView
+from birds import views
 
 
 urlpatterns = [
-    url(r'^$', BirdListView.as_view()),
-    url(r'^api/birds/(?P<pks>[\d,]+)/$', BirdDetailAPIViewAST.as_view(), name='birds_geojson'),
-    url(r'^birds/(?P<pks>[\d,]+)/$', BirdDetailViewAST.as_view()),
-    url(r'^api/birds/$', BirdListAPIView.as_view()),
-    url(r'^api/range/(?P<pk>\d+)/$', BirdRangeAPIViewAST.as_view()),
-    url(r'^birds/$', BirdListView.as_view(), name='bird_list'),
-    url(r'^world_borders/$', WorldBordersView.as_view()),
+    url(r'^$', views.BirdListView.as_view()),
+    url(r'^api/birds/(?P<pks>[\d,]+)/$', views.BirdDetailAPIViewAST.as_view(), name='birds_geojson'),
+    url(r'^birds/(?P<pks>[\d,]+)/$', views.BirdDetailViewAST.as_view()),
+    url(r'^api/birds/$', views.BirdListAPIView.as_view()),
+    url(r'^api/range/(?P<pk>\d+)/$', views.BirdRangeAPIViewAST.as_view()),
+    url(r'^birds/$', views.BirdListView.as_view(), name='bird_list'),
+    url(r'^world_borders/$', views.WorldBordersView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 ]
